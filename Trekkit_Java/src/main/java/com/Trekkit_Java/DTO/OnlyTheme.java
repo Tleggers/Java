7b1e2dc4fa23b1905d2585a4f6083a02879a1b1a
hyun0627 <quswpgjs21@naver.com>
2025-06-19 17:46:24 +0900

    2025-06-19 테마 완

---
+++ Trekkit_Java/src/main/java/com/Trekkit_Java/DTO/OnlyTheme.java
@@ -0,0 +1,17 @@
+package com.Trekkit_Java.DTO;
+
+import lombok.Data;
+
+@Data
+public class OnlyTheme {
+	
+	private Long id;
+    private String name;
+    private String subtitle;
+    private String description;
+    private String image_url;
+    private String time;
+    private String difficulty;
+    private int numberofcourse;
+
+}
