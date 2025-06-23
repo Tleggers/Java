56326d92772f3f2050fc0a03854a2397961c3e4d
kippeum02 <qpalzm4455@naver.com>
2025-06-23 16:21:17 +0900

    0623 16:20

--- Trekkit_Java/src/main/java/com/Trekkit_Java/DTO/MountainImageDTO.java
+++ Trekkit_Java/src/main/java/com/Trekkit_Java/DTO/MountainImageDTO.java
@@ -7,6 +7,10 @@ public class MountainImageDTO {
 	private Long id;
     private String name;
     private String region;
+    private String location;
+    private String height;
+    private String tip;
+    private String warning;
     private Double latitude;
     private Double longitude;
     private String image_url;
